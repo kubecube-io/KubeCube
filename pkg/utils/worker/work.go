be8875ce5e3c3fd42c2f6c43d9c8f57fa60b7da8
weilaaa <weilanidaye@gmail.com>
2022-01-10 15:51:41 +0800

    update license date

--- pkg/utils/worker/work.go
+++ pkg/utils/worker/work.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 KubeCube Authors
+
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+Copyright 2023 KubeCube Authors
 */
 
 package worker
