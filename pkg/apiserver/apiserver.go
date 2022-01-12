2dc682af3c3510e2f679f609b3c747be8268b707
tangxinxing <tangxinxing1994@gmail.com>
2022-01-12 10:13:12 +0800

    change search ingress domain restapi path

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -105,7 +105,7 @@ func registerCubeAPI(cfg *Config) http.Handler {
 		k8sApiExtend.Any("/clusters/:cluster/namespaces/:namespace/:resourceType/:resourceName", resourcemanage.ExtendHandle)
 		k8sApiExtend.Any("/clusters/:cluster/namespaces/:namespace/:resourceType", resourcemanage.ExtendHandle)
 		k8sApiExtend.POST("/clusters/:cluster/yaml/deploy", yamldeploy.Deploy)
-		k8sApiExtend.GET("ingressDomainSuffix/:cluster/:project", resourcemanage.IngressDomainSuffix)
+		k8sApiExtend.GET("/clusters/:cluster/projects/:project/ingressDomainSuffix", resourcemanage.IngressDomainSuffix)
 	}
 
 	return router
