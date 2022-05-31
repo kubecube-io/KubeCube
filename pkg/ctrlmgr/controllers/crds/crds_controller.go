6be6d41d3190fb5b112e85c162bab835dda34626
weilaaa <weilanidaye@gmail.com>
2022-05-31 17:16:12 +0800

    add crd rbac rules into rbac

--- pkg/ctrlmgr/controllers/crds/crds_controller.go
+++ pkg/ctrlmgr/controllers/crds/crds_controller.go
@@ -2,15 +2,16 @@ package crds
 
 import (
 	"context"
+	"k8s.io/apimachinery/pkg/types"
+
 	"github.com/kubecube-io/kubecube/pkg/clog"
+	rbacv1 "k8s.io/api/rbac/v1"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 type CrdReconciler struct {
@@ -31,7 +32,6 @@ func (r *CrdReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 
 	crd := v1.CustomResourceDefinition{}
 
-	// get cr ensure current cluster cr exist
 	if err := r.Get(ctx, req.NamespacedName, &crd); err != nil {
 		if errors.IsNotFound(err) {
 			return ctrl.Result{}, nil
@@ -43,7 +43,69 @@ func (r *CrdReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 }
 
 func (r *CrdReconciler) syncCrd(ctx context.Context, crd v1.CustomResourceDefinition) (ctrl.Result, error) {
+	roles, err := r.mutateClusterRole(ctx, crd)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if err = r.updateClusterRoles(ctx, roles); err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func makePolicyRule(resources, resourceNames, verbs []string) rbacv1.PolicyRule {
+	return rbacv1.PolicyRule{
+		APIGroups:     []string{"*"},
+		Resources:     resources,
+		ResourceNames: resourceNames,
+		Verbs:         verbs,
+	}
+}
+
+// mutateClusterRole aggregate new crd auth fot build-in ClusterRole
+func (r *CrdReconciler) mutateClusterRole(ctx context.Context, crd v1.CustomResourceDefinition) ([]*rbacv1.ClusterRole, error) {
+	// ClusterRoles need to update
+	var names []string
+	var verbs []string
+	resources := []string{crd.Spec.Names.Plural}
+
+	if crd.Spec.Scope == "Cluster" {
+		names = []string{"aggregate-to-platform-admin", "aggregate-to-reviewer", "aggregate-to-project-admin-cluster", "aggregate-to-tenant-admin-cluster"}
+	} else {
+		names = []string{"aggregate-to-platform-admin", "aggregate-to-reviewer", "aggregate-to-project-admin", "aggregate-to-tenant-admin"}
+	}
+
+	roles := make([]*rbacv1.ClusterRole, 0, len(names))
+
+	for _, name := range names {
+		role := &rbacv1.ClusterRole{}
+		if err := r.Get(ctx, types.NamespacedName{Name: name}, role); err != nil {
+			return nil, err
+		}
+		if name == "aggregate-to-platform-admin" || name == "aggregate-to-project-admin" || name == "aggregate-to-tenant-admin" {
+			verbs = []string{"get", "list", "watch", "create", "delete", "deletecollection", "update", "patch"}
+		}
+		if name == "aggregate-to-reviewer" || name == "aggregate-to-project-admin-cluster" || name == "aggregate-to-tenant-admin-cluster" {
+			verbs = []string{"get", "list", "watch"}
+		}
+		newPolicyRule := makePolicyRule(resources, nil, verbs)
+		// we do not need deduplication cause k8s responsible for
+		role.Rules = append(role.Rules, newPolicyRule)
+		roles = append(roles, role)
+	}
+
+	return roles, nil
+}
+
+func (r *CrdReconciler) updateClusterRoles(ctx context.Context, clusterRoles []*rbacv1.ClusterRole) error {
+	for _, clusterRole := range clusterRoles {
+		if err := r.Update(ctx, clusterRole); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -53,28 +115,7 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		return err
 	}
 
-	// filter update event
-	predicateFunc := predicate.Funcs{
-		CreateFunc: func(event event.CreateEvent) bool {
-			return true
-		},
-		UpdateFunc: func(updateEvent event.UpdateEvent) bool {
-			if !updateEvent.ObjectNew.GetDeletionTimestamp().IsZero() {
-				return true
-			}
-			return false
-		},
-		DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
-			return false
-		},
-		GenericFunc: func(genericEvent event.GenericEvent) bool {
-			// we use generic event to process init failed cluster
-			return true
-		},
-	}
-
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1.CustomResourceDefinition{}).
-		WithEventFilter(predicateFunc).
 		Complete(r)
 }
