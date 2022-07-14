238aee711af4f8c6121c7be5af2e774feda2ecdb
weilaaa <weilanidaye@gmail.com>
2022-07-14 18:58:33 +0800

    add v1.2.4 changelog

--- pkg/apis/cluster/v1/cluster_types.go
+++ pkg/apis/cluster/v1/cluster_types.go
@@ -72,7 +72,7 @@ type ClusterSpec struct {
 	// +optional
 	IngressDomainSuffix string `json:"ingressDomainSuffix,omitempty"`
 
-	//Is this cluster writable and if true then some resources such as workloads can be deployed on this cluster
+	// Is this cluster writable and if true then some resources such as workloads can be deployed on this cluster
 	// +optional
 	IsWritable bool `json:"isWritable,omitempty"`
 }
